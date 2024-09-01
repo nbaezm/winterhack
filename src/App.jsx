15e5aeda263944511d41b9635b109f710d93fa97
Nadia Báez <nebaez93@gmail.com>
2024-09-01 13:24:37 -0300

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import ComponenteNuevo from './ComponenteNuevo'
+
+
+function App() {
+  
+   console.log("HOLA DESDE REACT")
+
+  return (
+    <>
+    <h1>Hola Mundo desde React</h1>
+    <p>Te queremos Vite y React</p>
+    <ComponenteNuevo></ComponenteNuevo>
+
+    </>
+  )
+}
+
+export default App
